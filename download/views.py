315a0bd99d40be37ca2bdb393ce71de4944f2cc3
SYED ALTAMASH ALI MUNAWWAR ALI <143215243+AltamashAliSyed@users.noreply.github.com>
2026-05-26 20:28:45 +0530

    Update views.py

--- download/views.py
+++ download/views.py
@@ -18,7 +18,7 @@ def home(request):
                 info = ydl.extract_info(video_link,download=True)
                 video_url=info.get("title")+".mp4"
         except Exception as e:
-             error = str(e)
+             error = "download failed"
 
    
     
